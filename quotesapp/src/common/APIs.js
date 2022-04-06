e0628af6443209ce435eb5e108358d4fae0ad4fd
TheRealYaseen <s.syedyaseen.s@gmail.com>
2022-04-06 09:35:32 +0100

    Update info

--- quotesapp/src/common/APIs.js
+++ quotesapp/src/common/APIs.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const weatherAPIKey = 'c221472a05e4c098df3d9eb79887c2f5'
+export const weatherAPIKey = 'YourAPIHERE'
 
 export const weatherAPI =  axios.create({
   baseURL: "https://api.openweathermap.org/data/2.5/weather",
